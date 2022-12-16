0740617c4d0512de53f4991bf723e318f311d522
Raegen <120728359+Raegen2413@users.noreply.github.com>
2022-12-16 14:23:27 +0000

    modified:   index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import * as THREE from "./three.js/build/three.module.js"
+import {OrbitControls} from "./three.js/examples/jsm/controls/OrbitControls.js"
+
+var scene, fixedCam, freeCam, renderer, control, mouse
+
+function init() {
+    scene = new THREE.Scene()
+
+    
+}
